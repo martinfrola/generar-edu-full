8c1ccaa411f7d0c3395d41b3977011e59814cee2
martinfrola <martinfrola@gmail.com>
2021-12-03 21:17:48 -0300

    prueba bootstrap

--- generar-edu/src/index.js
+++ generar-edu/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./sass/app.scss";
-import "./_bootstrap.scss";
+import "./bootstrap.scss";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Routes from "./routes/Routes";
